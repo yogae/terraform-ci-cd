b1eaa4dc89074f2651f4b36927b9fe043dd95126
kim jihyun <>
2019-08-20 02:25:39 +0900

    fix

--- variables.tf
+++ variables.tf
@@ -18,26 +18,6 @@ variable "codebuild_s3_bucket_name" {
     type = string
 }
 
-variable "forbidden_account_ids" {
-    type = list(string)
-    default = ""
-}
-
-variable "allowed_account_ids" {
-    type = list(string)
-    default = ""
-}
-
-variable "aws_access_key" {
-    type = string
-    default = ""
-}
-
-variable "aws_secret_key" {
-    type = string
-    default = ""
-}
-
 variable "artifacts_type" {
     type = string
     default = "NO_ARTIFACTS"
